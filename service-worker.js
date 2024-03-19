b3216b51bc0e33491377e4a5ec89bdd738e4e078
Lucas Gomes <lucasilgo@hotmail.com>
2024-03-19 14:51:52 -0300

    Add service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
-// TODO
+self.addEventListener('install', (event) => { // event when service worker install
+    console.log( 'install', event);
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => { // event when service worker activated
+    console.log('activate', event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) { // HTTP request interceptor
+    event.respondWith(fetch(event.request)); // send all http request without any cache logic
+    // // TODO: Enable cache to use the PWA offline
+    // event.respondWith(
+    //     caches.match(event.request).then(function(response) {
+    //         return response || fetch(event. request);
+    //     })
+    // );
+});
