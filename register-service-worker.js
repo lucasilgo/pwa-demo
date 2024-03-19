9f0f6884195b1543205edf81a41ffb4e197bb006
Lucas Gomes <lucasilgo@hotmail.com>
2024-03-19 16:11:51 -0300

    Test service worker

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,5 +1,30 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js');
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('https://lucasilgo.github.io/pwa-demo/service-worker.js');
+
+        this.document.getElementById('push-notif').addEventListener('click', () => {
+            // displayNotification('Button push notification clicked!')
+            console.log('AAAAAA')
+        });
     });
 }
+
+if ('Notification' in window && Notification.permission != 'granted') {
+    Notification.requestPermission(status => {
+        displayNotification('Push Notification Enabled');
+    });
+}
+
+const displayNotification = notificationTitle => {
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(reg => {
+            const options = {
+                body: 'Push notifications are enabled!',
+                icon: '/assets/icons/icon-512x512.png',
+                vibrate: [300, 400, 300]
+            };
+
+            reg.showNotification(notificationTitle, options);
+        });
+    }
+};
